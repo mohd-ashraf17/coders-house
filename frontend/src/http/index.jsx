9653d84107b6bf816af64ec1786baf4bfc25a263
Mohdashraf7078 <mohdashraf1217@gmail.com>
2024-02-09 06:54:36 -0800

    fix

--- frontend/src/http/index.jsx
+++ frontend/src/http/index.jsx
@@ -7,8 +7,6 @@ const api = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
   },
-  withCredentials: true,
-  credentials: 'same-origin',
 });
 // axios.defaults.withCredentials = true;
 
