0cf0fc2bd1ea7fda6b36a934f6c34f2d0dd51234
Mohdashraf7078 <mohdashraf1217@gmail.com>
2024-02-09 05:31:14 -0800

    add cred

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const PORT =process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 require('./db')();
 const Router = require('./routes')
 const cookieParser = require('cookie-parser')
@@ -13,7 +13,7 @@ app.use(cookieParser())
 //     origin: ['http://localhost:5173']
 // }
 // app.use(cors(corsOption))
-app.use(cors());
+app.use(cors({ credentials: 'true' }));
 app.use(express.json({ limit: '10mb' }))
 app.use(Router)
 const server = app.listen(PORT, () => console.log(`running on PORT ${PORT}`));
